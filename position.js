6e0b66fcaea2ca3bf74acfcbab46cdd18d8429a6
Administrator <zhaopanhw@foxmail.com>
2018-10-29 16:24:13 +0800

    commit all

---
+++ position.js
@@ -0,0 +1,332 @@
+var positionData= {
+    code: 1,
+    msg: "获取成功！",
+    dataList: [
+        {
+            //直流柜
+            type:"dc_cabinet",
+            dc_cabinet: [
+                {x: -90, y: 40},
+                {x: -90, y: 34},
+                {x: -90, y: 28},
+                {x: -90, y: 22},
+                {x: -90, y: 16},
+                {x: -90, y: 10},
+                {x: -90, y: 4},
+                {x: -90, y: -2},
+                {x: -90, y: -8},
+
+
+
+                {x: -50, y: 40},
+                {x: -50, y: 34},
+                {x: -50, y: 28},
+                {x: -50, y: 22},
+                {x: -50, y: 16},
+                {x: -50, y: 10},
+                {x: -50, y: 4},
+                {x: -50, y: -2},
+                {x: -50, y: -8},
+
+                {x: -10, y: 40},
+                {x: -10, y: 34},
+                {x: -10, y: 28},
+                {x: -10, y: 22},
+                {x: -10, y: 16},
+                {x: -10, y: 10},
+                {x: -10, y: 4},
+                {x: -10, y: -2},
+                {x: -10, y: -8},
+
+
+                {x: 30, y: 40},
+                {x: 30, y: 34},
+                {x: 30, y: 28},
+                {x: 30, y: 22},
+                {x: 30, y: 16},
+                {x: 30, y: 10},
+                {x: 30, y: 4},
+                {x: 30, y: -2},
+                {x: 30, y: -8},
+
+                {x: 70, y: 40},
+                {x: 70, y: 34},
+                {x: 70, y: 28},
+                {x: 70, y: 22},
+                {x: 70, y: 16},
+                {x: 70, y: 10},
+                {x: 70, y: 4},
+                {x: 70, y: -2},
+                {x: 70, y: -8},
+
+
+            ]
+        },
+        {
+            //交流柜
+            type:"ac_cabinet",
+            ac_cabinet: [
+                {x: -80, y: -25},
+                {x: -65, y: -25},
+                {x: -50, y: -25},
+                {x: -35, y: -25},
+
+                {x: -20, y: -25},
+                {x: -5, y: -25},
+                {x: 10, y: -25},
+                {x: 25, y: -25},
+                {x: 40, y: -25},
+                {x: 55, y: -25},
+
+
+
+                {x: -80, y: -31},
+                {x: -65, y: -31},
+                {x: -50, y: -31},
+                {x: -35, y: -31},
+
+                {x: -20, y: -31},
+                {x: -5, y: -31},
+                {x: 10, y: -31},
+                {x: 25, y: -31},
+                {x: 40, y: -31},
+                {x: 55, y: -31},
+
+
+            ]
+        },
+        {
+            //电池
+            type:"battery",
+            battery: [
+                {fx:"cloum",x: -124, y:46},
+                {fx:"cloum",x: -124, y:18},
+
+                {fx:"row",x: -116, y:-55},
+                {fx:"row",x: -88    , y:-55}
+
+            ]
+        },
+        {
+            //空调
+            type:"kongtiao",
+            kongtiao: [
+                {fx:"cloum",x: -100, y:56},
+                {fx:"cloum",x: -50, y:56},
+                {fx:"youshang",x: 123, y:53},
+                {fx:"zuoxia",x: 123, y:53},
+
+
+            ]
+        }
+
+    ]
+}
+
+
+var positionDatas= {
+    code: 1,
+    msg: "获取成功！",
+    dataList: [
+        {
+            //odf架子
+            type:"odf",
+            odf: [
+                {top: 150, left: 160},
+                {top: 150, left: 210},
+
+                {top: 250, left: 160},
+                {top: 250, left: 210},
+
+                {top: 350, left: 160},
+                {top: 350, left: 210},
+            ]
+        },
+        {
+            //分光器
+            type:"fenguang",
+            fenguang: [
+                {top: 500, left: 160},
+                {top: 550, left: 160},
+            ]
+        },
+        {
+            //直流柜
+            type:"dc_cabinet",
+            dc_cabinet: [
+                {top: 150, left: 400},
+                {top: 200, left: 400},
+                {top: 250, left: 400},
+                {top: 300, left: 400},
+                {top: 350, left: 400},
+                {top: 400, left: 400},
+                {top: 450, left: 400},
+                {top: 500, left: 400},
+                {top: 550, left: 400},
+
+
+                {top: 150, left: 640},
+                {top: 200, left: 640},
+                {top: 250, left: 640},
+                {top: 300, left: 640},
+                {top: 350, left: 640},
+                {top: 400, left: 640},
+                {top: 450, left: 640},
+                {top: 500, left: 640},
+                {top: 550, left: 640},
+
+
+                {top: 150, left: 880},
+                {top: 200, left: 880},
+                {top: 250, left: 880},
+                {top: 300, left: 880},
+                {top: 350, left: 880},
+                {top: 400, left: 880},
+                {top: 450, left: 880},
+                {top: 500, left: 880},
+                {top: 550, left: 880},
+
+
+                {top: 150, left: 1120},
+                {top: 200, left: 1120},
+                {top: 250, left: 1120},
+                {top: 300, left: 1120},
+                {top: 350, left: 1120},
+                {top: 400, left: 1120},
+                {top: 450, left: 1120},
+                {top: 500, left: 1120},
+                {top: 550, left: 1120},
+
+
+            ]
+        },
+        {
+            //交流柜
+            type:"ac_cabinet",
+            ac_cabinet: [
+                {top: 700, left: 180},
+                {top: 750, left: 180},
+
+                {top: 700, left: 280},
+                {top: 750, left: 280},
+
+                {top: 700, left: 380},
+                {top: 750, left: 380},
+
+                {top: 700, left: 480},
+                {top: 750, left: 480},
+
+                {top: 700, left: 580},
+                {top: 750, left: 580},
+
+                {top: 700, left: 680},
+                {top: 750, left: 680},
+
+                {top: 700, left: 780},
+                {top: 750, left: 780},
+
+                {top: 700, left: 880},
+                {top: 750, left: 880},
+
+                {top: 700, left: 980},
+                {top: 750, left: 980},
+
+                {top: 700, left: 1080},
+                {top: 750, left: 1080},
+            ]
+        },
+        {
+            //电池
+            type:"battery",
+            battery: [
+                {top: 0, left: 0,fx:"vertical"},
+                {top: 100, left: 0,fx:"vertical"},
+                {top: 1000, left: 100,fx:"row"},
+                {top: 1000, left: 200,fx:"row"},
+                {top: 1000, left: 1100,fx:"big"}
+
+            ]
+        },
+        {
+            //空调
+            type:"kongtiao",
+            kongtiao: [
+                {fx:"cloum",top: -55, left: 0,groupTop:0,groupLeft:120},
+                {fx:"cloum",top: -50, left: 0,groupTop:0,groupLeft:680},
+                {fx:"youshang",top: -30, left: 50,groupTop:0,groupLeft:1285},
+                {fx:"youxia",top: 0, left: 110,groupTop:950,groupLeft:1290},
+
+            ]
+        },
+        {
+            //温湿度
+            type:"shidu",
+            shidu: [
+                {fx:"top",top: -10, left: 60,groupTop:0,groupLeft:450},
+                {fx:"top",top: -10, left: 60,groupTop:0,groupLeft:1100},
+
+
+                {fx:"bottom",top: 30, left: 30,groupTop:300,groupLeft:280},
+                {fx:"bottom",top: 30, left: 30,groupTop:300,groupLeft:525},
+                {fx:"bottom",top: 30, left: 30,groupTop:300,groupLeft:765},
+                {fx:"bottom",top: 30, left: 30,groupTop:300,groupLeft:1005},
+                {fx:"bottom",top: 30, left:-50,groupTop:450,groupLeft:0},
+                {fx:"bottom",top: -10, left:100,groupTop:660,groupLeft:800},
+                {fx:"bottom",top: -10, left:100,groupTop:760,groupLeft:1270},
+                {fx:"bottom",top: -10, left:-30,groupTop:800,groupLeft:700},
+            ]
+        },
+        {
+            //摄像
+            type:"shexiang",
+            shexiang: [
+                {fx:"bottom",top: -60, left:10,groupTop:0,groupLeft:1200},
+                {fx:"bottom",top: -60, left:10,groupTop:150,groupLeft:80},
+                {fx:"bottom",top: -60, left:0,groupTop:150,groupLeft:260},
+                {fx:"bottom",top: -60, left:0,groupTop:150,groupLeft:500},
+                {fx:"bottom",top: -60, left:0,groupTop:150,groupLeft:740},
+                {fx:"bottom",top: -60, left:0,groupTop:150,groupLeft:980},
+
+                {fx:"top",top: 80, left:60,groupTop:520,groupLeft:340},
+                {fx:"top",top: 80, left:60,groupTop:520,groupLeft:580},
+                {fx:"top",top: 80, left:60,groupTop:520,groupLeft:820},
+                {fx:"top",top: 80, left:60,groupTop:520,groupLeft:1060},
+
+                {fx:"heng",top: 40, left:-80,groupTop:620,groupLeft:0},
+
+                {fx:"youxia",top: 60, left:60,groupTop:620,groupLeft:1340},
+
+                {fx:"heng",top: 40, left:-80,groupTop:870,groupLeft:180},
+            ]
+        },
+        {
+            //烟感
+            type:"yangan",
+            yangan: [
+                {fx:"bottom",top: 30, left:10,groupTop:150,groupLeft:343},
+                {fx:"bottom",top: 30, left:10,groupTop:150,groupLeft:583},
+                {fx:"bottom",top: 30, left:10,groupTop:150,groupLeft:823},
+                {fx:"bottom",top: 30, left:10,groupTop:150,groupLeft:1063},
+
+                {fx:"bottom",top: 30, left:0,groupTop:300,groupLeft:0},
+
+                {fx:"bottom",top: 30, left:0,groupTop:530,groupLeft:263},
+                {fx:"bottom",top: 30, left:0,groupTop:530,groupLeft:503},
+                {fx:"bottom",top: 30, left:0,groupTop:530,groupLeft:743},
+                {fx:"bottom",top: 30, left:0,groupTop:530,groupLeft:983},
+                {fx:"bottom",top: 30, left:10,groupTop:530,groupLeft:1340},
+
+                {fx:"bottom",top: 30, left:10,groupTop:530,groupLeft:1340},
+
+                {fx:"bottom",top: -40, left:0,groupTop:800,groupLeft:343},
+                {fx:"bottom",top: -40, left:0,groupTop:800,groupLeft:1033},
+
+                {fx:"bottom",top: 30, left:0,groupTop:980,groupLeft:720},
+
+            ]
+        }
+
+    ]
+}
+
+exports=positionData,positionDatas;
